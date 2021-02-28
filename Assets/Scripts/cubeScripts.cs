4a2803d797fd9c701477032693b24f14ffb19910
furkan selçuk <furkanselcuk11@gmail.com>
2021-02-28 15:37:37 +0300

    +add: Cube Script

---
+++ Assets/Scripts/cubeScripts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScripts : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+    // Script'in bağlı oldupu nesnenin Rigibody özelliğine erişir...
+    rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // Tuşa basıldığında yapılacak işlem
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Kuvvet uygular
+            rb.AddForce(new Vector3(0, 20.5f, 0));
+        }
+    }
+}
