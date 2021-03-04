e93cdfd324761c28cb64e51d42432888c69e5f7d
furkan selçuk <furkanselcuk11@gmail.com>
2021-03-04 12:43:36 +0300

    +add: collision with OnTriggerEnter

---
+++ Assets/Scripts/BallOnTriggerEnterControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallOnTriggerEnterControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+        
+    void Update()
+    {
+        
+    }
+
+    // Temas eden objeyi belirler
+    void OnTriggerEnter(Collider go)
+    {
+        Debug.Log("Kutu içerisine giren obje:"+go.name);
+    }
+}
