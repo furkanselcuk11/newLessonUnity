f71005df6aadd0dd8f72be550ad9c6bd7b7dd99c
furkan selçuk <furkanselcuk11@gmail.com>
2021-03-05 11:03:47 +0300

    +add: characterController added

---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float characterSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+        void Update()
+    {
+        // Karakterşn yönü ve hızını belirleme
+        float zDirection = Input.GetAxis("Vertical") * characterSpeed;
+        float xDirection = Input.GetAxis("Horizontal") * characterSpeed;
+        zDirection *= Time.deltaTime;
+        xDirection *= Time.deltaTime;
+        transform.Translate(xDirection, 0, zDirection);
+        
+    }
+}
