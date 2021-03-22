9e04dff5658a96ed0471becd55b2f5c8d86205cc
furkan selçuk <furkanselcuk11@gmail.com>
2021-03-23 00:18:27 +0300

    +add: zombie animator

---
+++ Assets/zombieController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieController : MonoBehaviour
+{
+    public Animator anim;
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("run");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("idle");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetTrigger("attack");
+        }
+
+    }
+}
