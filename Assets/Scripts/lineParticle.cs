a74ac5273e95749c77b3464cfc6c76a09a529ba7
furkan selçuk <furkanselcuk11@gmail.com>
2021-03-23 19:56:43 +0300

    add: lineParticle adding

---
+++ Assets/Scripts/lineParticle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineParticle : MonoBehaviour
+{
+    LineRenderer line;
+
+    Transform Cube_1;
+    Transform Cube_2;
+    Transform Cube_3;
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+
+        Cube_1 = GameObject.Find("Cube_1").transform;
+        Cube_2 = GameObject.Find("Cube_2").transform;
+        Cube_3 = GameObject.Find("Cube_3").transform;
+
+        line.SetPosition(0, Cube_1.position);
+        line.SetPosition(1, Cube_2.position);
+        line.SetPosition(2, Cube_3.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
