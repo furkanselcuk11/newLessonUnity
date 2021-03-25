c021f297c08295fe57fb08e608d50e32c5568e4c
furkan selçuk <furkanselcuk11@gmail.com>
2021-03-25 21:33:46 +0300

    +add: foe tracking adding

---
+++ Assets/Scripts/foeTracking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class foeTracking : MonoBehaviour
+{
+    public NavMeshAgent foe;
+    Transform player;
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        foe.destination=player.position; // Nesneyi takip eder
+    }
+}
