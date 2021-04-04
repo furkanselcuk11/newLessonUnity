6614a45631cc6370bd4538548f9ab839f85033f7
furkan selçuk <furkanselcuk11@gmail.com>
2021-04-04 21:27:39 +0300

    +add: playerContoller script added

--- Assets/Scripts/FPSController/playerController.cs
+++ Assets/Scripts/FPSController/playerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    CharacterController controller;
+    Vector3 velocity;
+    bool isGrounded;
 
-    // Update is called once per frame
-    void Update()
+    public Transform ground;
+    public float distance = 0.3f;
+
+    public float speed;
+    public float JumpHeight;
+    public float gravity;
+
+    public LayerMask mask;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        #region Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move*speed*Time.deltaTime);
+        #endregion
+
+        #region Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y += Mathf.Sqrt(JumpHeight * -3.0f * gravity);
+        }
+        #endregion
+
+        #region Gravity
+        isGrounded = Physics.CheckSphere(ground.position, distance, mask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        #endregion
     }
 }
